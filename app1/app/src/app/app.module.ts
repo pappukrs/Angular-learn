c836fdc1ef540e389ebf9b11308fb65d83f2e151
pappukrs <digitalpappu101@gmail.com>
2023-03-05 17:48:34 +0530

    app1-header-footer-side-nav-main-body

---
+++ app1/app/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NewComponentComponent } from './new-component/new-component.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { MainBodyComponent } from './main-body/main-body.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NewComponentComponent,
+    HeaderComponent,
+    FooterComponent,
+    SideNavComponent,
+    MainBodyComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
